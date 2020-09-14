66fc5e0ea2858af7ba5143be2ad3ed621c2a8d81
Timothy Otieno <timothyking094@gmail.com>
2020-09-14 18:21:26 +0300

    Modified the Navigation Section

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React, { Fragment } from "react";
 import "./App.css";
-
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-
 //Pages Import
 import { Home, SingleRoom, Rooms, Error } from "./pages/exportFiles";
+//Components Import
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <Fragment>
       <Router>
+        <Navbar />
         <Switch>
           <Route exact path='/' component={Home} />
           <Route exact path='/rooms' component={Rooms} />
