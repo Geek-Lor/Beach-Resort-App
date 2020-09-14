8a051209571f1d115887006ec5014c86edbaa96e
Timothy Otieno <timothyking094@gmail.com>
2020-09-14 17:25:07 +0300

    Application set-up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from "react";
+import "./App.css";
+
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+//Pages Import
+import { Home, SingleRoom, Rooms, Error } from "./pages/exportFiles";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <Router>
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/rooms' component={Rooms} />
+          <Route exact path='/rooms/:lord' component={SingleRoom} />
+          <Route component={Error} />
+        </Switch>
+      </Router>
+    </Fragment>
   );
 }
 
